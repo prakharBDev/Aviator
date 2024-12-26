c9bde90bc22bd9e16b05736423f158f080196532
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-12-26 14:17:34 +0530

    File added

---
+++ aviator-game/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AviatorUI from "./components/AviatorUI";
+
+function App() {
+  return (
+    <div className="App">
+      <AviatorUI />
+    </div>
+  );
+}
+
+export default App;
