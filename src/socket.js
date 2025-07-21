ac21184087b0b5e5d0ac1c7b3e9de49afb6e2929
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2025-07-21 22:40:46 +0530

    final changes

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // Backend server URL
-const SOCKET_URL = "http://localhost:5001";
+const SOCKET_URL = "https://aviator-8.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   transports: ["websocket"],
